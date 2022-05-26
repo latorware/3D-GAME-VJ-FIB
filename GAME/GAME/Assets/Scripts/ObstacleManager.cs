fa0954bb49fb0b54e74292e4b1203f6a9380fe58
david latorre <davidlatorremotos@gmail.com>
2022-05-26 22:32:34 +0200

    progres

--- GAME/GAME/Assets/Scripts/ObstacleManager.cs
+++ GAME/GAME/Assets/Scripts/ObstacleManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleManagerLevel2 : MonoBehaviour
+public class ObstacleManager : MonoBehaviour
 {
     public GlobalVolumeManager volumeManager;
 
